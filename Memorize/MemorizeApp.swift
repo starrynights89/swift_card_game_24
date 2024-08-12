32375442c2242a241f261a2662a04495b8121b9c
Alexander Hartson <alexander-hartson@outlook.com>
2024-08-12 14:27:21 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexander Hartson on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
